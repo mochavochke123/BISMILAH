4d8b48d45175cc664773b56ea6b3ac3a6c604953
User <user@example.com>
2026-04-26 18:20:22 +0300

    Reorganize release into images/music/icons folders

--- release/install.bat
+++ release/install.bat
@@ -9,7 +9,7 @@ echo Set WshShell = CreateObject("WScript.Shell") > temp.vbs
 echo Set Shortcut = WshShell.CreateShortcut("%USERPROFILE%\Desktop\BISMILAH.lnk") >> temp.vbs
 echo Shortcut.TargetPath = "%~dp0gaame.exe" >> temp.vbs
 echo Shortcut.WorkingDirectory = "%~dp0" >> temp.vbs
-echo Shortcut.IconLocation = "%~dp0bis.ico" >> temp.vbs
+echo Shortcut.IconLocation = "%~dp0icons\bis.ico" >> temp.vbs
 echo Shortcut.Description = "BISMILAH Game" >> temp.vbs
 echo Shortcut.Save() >> temp.vbs
 
@@ -19,7 +19,7 @@ del temp.vbs
 
 :: Проверяем
 if exist "%USERPROFILE%\Desktop\BISMILAH.lnk" (
-    echo [OK] Shortcut created!
+    echo [OK] Shortcut created with icon!
 ) else (
     echo [!] Could not create shortcut
 )
